d23264d01afda91249e51285c073ced0bb3418a5
JanRitschel <jrls2@live.de>
2024-05-03 17:02:29 +0200

    moved everything one level up, almost finished dnf
    added next_or, next_and, find_associated_bracket and some documentation.
    also removed redundant folder and moved all python stuff up one level

---
+++ newOrganisation/sample/model/DisjunctiveNormalForm.py
@@ -0,0 +1,134 @@
+"""
+UNKLAR: Warum ist das eine Klasse, welche Vorteile hat das? Es sollte auch als File funktionieren.
+"""
+
+
+def compute(expression:str) -> str:
+    """
+    Return a Disjunctive Normal Form of the expression as a String.
+
+    The resulting String uses "," as "or"/Disjunction and "&" as 
+    "and"/Conjunction and does not contain any brackets.
+    The input-expression can contain the logical operators:
+        "or" as ","
+        "and" as "&"
+        brackets as "(" and ")" 
+    Everything else will be treated as elements.
+    There cannot be any whitespaces in the input-expression.
+
+    Parameters:
+        expression  (str): A logical expression with brackets to be turned
+        into a DNF without brackets
+
+    Returns:
+
+    """
+    return ""
+
+def recurse(expression:str) -> list[str]:
+    last_pos = len(expression) - 1
+    if expression.startswith("("):
+        associated_closed_bracket = find_associated_closed_bracket(expression, 0)
+        if associated_closed_bracket == last_pos:
+            return recurse(expression[1:associated_closed_bracket])
+        else:
+            next_outside_operator = expression[associated_closed_bracket + 1]
+            if next_outside_operator == ",":
+                return union(recurse(expression[1:associated_closed_bracket]), recurse(expression[associated_closed_bracket + 1:last_pos]))
+            if next_outside_operator == "&":
+                return product(recurse(expression[1:associated_closed_bracket]), recurse(expression[associated_closed_bracket + 1:last_pos]))
+    else:
+        next_inside_operator = next_or(expression, 0)
+        if next_inside_operator > 0:
+            return union(recurse(expression[1:next_inside_operator]), recurse(expression[next_inside_operator + 1:last_pos]))
+        next_inside_operator = next_and(expression, 0)
+        if next_inside_operator > 0:
+            return product(recurse(expression[1:next_inside_operator]), recurse(expression[next_inside_operator + 1:last_pos]))
+
+    return [expression] #FEHLERANFÄLLIG
+
+def union(tree_a:list, tree_b:list) -> list:
+    tree_a.extend(tree_b)
+    return tree_a
+
+def product(tree_a:list, tree_b:list) -> list:
+    res = []
+    for content_a in tree_a:
+        for content_b in tree_b:
+            if content_a == content_b:
+                res.append(content_a)
+            else:
+                res.append(content_a + "&" + content_b)
+    return res
+
+def next_or(expression:str, start_pos:int) -> int:
+    """
+    Return the postion of the next "," after "start_pos" in "expression".
+    
+    If no "," is found returns -1 instead.
+    If no "," is found before the next open bracket "(" returns -1 as well.
+
+    Parameters:
+        expression  (str): The string to be searched
+        start_pos   (int): The position at which searching starts.
+
+    Returns:
+        start_pos   (int): Position of the next ","
+        -1          (int): Returned if no relevant "," is found
+    """
+    end_pos = len(expression) - 1
+    while start_pos <= end_pos:
+        if expression[start_pos] == ",": return start_pos
+        elif expression[start_pos] == "(": return -1
+        start_pos += 1
+    
+    return -1
+
+def next_and(expression:str, start_pos:int) -> int:
+    """
+    Return the postion of the next "&" after "start_pos" in "expression".
+    
+    If no "&" is found returns -1 instead.
+    If no "&" is found before the next open bracket "(" returns -1 as well.
+
+    Parameters:
+        expression  (str): The string to be searched
+        start_pos   (int): The position at which searching starts.
+
+    Returns:
+        start_pos   (int): Position of the next "&"
+        -1          (int): Returned if no relevant "&" is found
+    """
+    end_pos = len(expression) - 1
+    while start_pos <= end_pos:
+        if expression[start_pos] == ",": return start_pos
+        elif expression[start_pos] == "(": return -1
+        start_pos += 1
+    
+    return -1
+
+def find_associated_closed_bracket(expression:str, start_pos:int) -> int:
+    """
+    Return position of associated bracket of the open bracket at "start_pos".
+
+    If "start_pos" is not the position of an open bracket returns start_pos.
+    If no closed bracket is found after an open bracket returns -1 instead.
+
+    Parameters:
+        expression  (str): The string to be analyzed
+        start_pos   (int): The position at which analysis starts.
+
+    Returns:
+        start_pos   (int): Either the initial position or the position of
+        the associated closed bracket.
+        -1          (int): Returned if the expression doesn't contain an
+        associated bracket
+    """
+    end_pos = len(expression) - 1
+    depth = 0
+    while start_pos <= end_pos:
+        if expression[start_pos] == "(": depth += 1
+        elif expression[start_pos] == ")": depth -= 1
+        if depth == 0: return start_pos
+        start_pos += 1
+    return -1
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import re
+import newOrganisation.sample.model.DisjunctiveNormalForm as dnf
 
 """ catalysts="[a, b, c * d,   (e f)]".strip()
 catalysts = re.sub("\\|", ",", catalysts)
@@ -9,6 +10,16 @@ catalysts = re.sub("\\s*&\\s*", "&", catalysts)
 catalysts = re.sub("\\s*,\\s*", ",", catalysts)
 catalysts = re.sub("\\s+", ",", catalysts) """
 
+var = "d,e,(a&b)"
+tree_a = ["a", "b"]
+tree_b = ["c", "d"]
+res = []
+""" while len(res) < len(var):
+    res.append(dnf.next_or(var, len(res)))
 
-
-print(set(["a", "b", "c", "d", "e"]))
+print(len(var))
+ """
+print(dnf.recurse(var))
+print(dnf.find_associated_closed_bracket(" ((())", 0))
+#print(dnf.recurse(var))
+#print(res)
